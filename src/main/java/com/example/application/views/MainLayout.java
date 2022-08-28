77a73ae5791decab823a32cf2cd7785bb85456ad
start.vaadin.com <no-reply@vaadin.com>
2022-08-28 07:34:16 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/MainLayout.java
@@ -0,0 +1,166 @@
+package com.example.application.views;
+
+import com.example.application.components.appnav.AppNav;
+import com.example.application.components.appnav.AppNavItem;
+import com.example.application.data.entity.User;
+import com.example.application.security.AuthenticatedUser;
+import com.example.application.views.about.AboutView;
+import com.example.application.views.addressform.AddressFormView;
+import com.example.application.views.chat.ChatView;
+import com.example.application.views.collaborativemasterdetail.CollaborativeMasterDetailView;
+import com.example.application.views.creditcardform.CreditCardFormView;
+import com.example.application.views.dashboard.DashboardView;
+import com.example.application.views.helloworld.HelloWorldView;
+import com.example.application.views.list.ListView;
+import com.example.application.views.masterdetail.MasterDetailView;
+import com.example.application.views.personform.PersonFormView;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.contextmenu.ContextMenu;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Footer;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.server.auth.AccessAnnotationChecker;
+import java.util.Optional;
+
+/**
+ * The main view is a top-level placeholder for other views.
+ */
+public class MainLayout extends AppLayout {
+
+    private H1 viewTitle;
+
+    private AuthenticatedUser authenticatedUser;
+    private AccessAnnotationChecker accessChecker;
+
+    public MainLayout(AuthenticatedUser authenticatedUser, AccessAnnotationChecker accessChecker) {
+        this.authenticatedUser = authenticatedUser;
+        this.accessChecker = accessChecker;
+
+        setPrimarySection(Section.DRAWER);
+        addToNavbar(true, createHeaderContent());
+        addToDrawer(createDrawerContent());
+    }
+
+    private Component createHeaderContent() {
+        DrawerToggle toggle = new DrawerToggle();
+        toggle.addClassNames("view-toggle");
+        toggle.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+        toggle.getElement().setAttribute("aria-label", "Menu toggle");
+
+        viewTitle = new H1();
+        viewTitle.addClassNames("view-title");
+
+        Header header = new Header(toggle, viewTitle);
+        header.addClassNames("view-header");
+        return header;
+    }
+
+    private Component createDrawerContent() {
+        H2 appName = new H2("My App");
+        appName.addClassNames("app-name");
+
+        com.vaadin.flow.component.html.Section section = new com.vaadin.flow.component.html.Section(appName,
+                createNavigation(), createFooter());
+        section.addClassNames("drawer-section");
+        return section;
+    }
+
+    private AppNav createNavigation() {
+        // AppNav is not yet an official component.
+        // For documentation, visit https://github.com/vaadin/vcf-nav#readme
+        AppNav nav = new AppNav();
+        nav.addClassNames("app-nav");
+
+        if (accessChecker.hasAccess(HelloWorldView.class)) {
+            nav.addItem(new AppNavItem("Hello World", HelloWorldView.class, "la la-globe"));
+
+        }
+        if (accessChecker.hasAccess(AboutView.class)) {
+            nav.addItem(new AppNavItem("About", AboutView.class, "la la-file"));
+
+        }
+        if (accessChecker.hasAccess(ListView.class)) {
+            nav.addItem(new AppNavItem("List", ListView.class, "la la-th"));
+
+        }
+        if (accessChecker.hasAccess(DashboardView.class)) {
+            nav.addItem(new AppNavItem("Dashboard", DashboardView.class, "la la-chart-area"));
+
+        }
+        if (accessChecker.hasAccess(ChatView.class)) {
+            nav.addItem(new AppNavItem("Chat", ChatView.class, "la la-comments"));
+
+        }
+        if (accessChecker.hasAccess(MasterDetailView.class)) {
+            nav.addItem(new AppNavItem("Master-Detail", MasterDetailView.class, "la la-columns"));
+
+        }
+        if (accessChecker.hasAccess(CollaborativeMasterDetailView.class)) {
+            nav.addItem(new AppNavItem("Collaborative Master-Detail", CollaborativeMasterDetailView.class,
+                    "la la-columns"));
+
+        }
+        if (accessChecker.hasAccess(PersonFormView.class)) {
+            nav.addItem(new AppNavItem("Person Form", PersonFormView.class, "la la-user"));
+
+        }
+        if (accessChecker.hasAccess(AddressFormView.class)) {
+            nav.addItem(new AppNavItem("Address Form", AddressFormView.class, "la la-map-marker"));
+
+        }
+        if (accessChecker.hasAccess(CreditCardFormView.class)) {
+            nav.addItem(new AppNavItem("Credit Card Form", CreditCardFormView.class, "la la-credit-card"));
+
+        }
+
+        return nav;
+    }
+
+    private Footer createFooter() {
+        Footer layout = new Footer();
+        layout.addClassNames("app-nav-footer");
+
+        Optional<User> maybeUser = authenticatedUser.get();
+        if (maybeUser.isPresent()) {
+            User user = maybeUser.get();
+
+            Avatar avatar = new Avatar(user.getName(), user.getProfilePicture());
+            avatar.addClassNames("me-xs");
+
+            ContextMenu userMenu = new ContextMenu(avatar);
+            userMenu.setOpenOnClick(true);
+            userMenu.addItem("Logout", e -> {
+                authenticatedUser.logout();
+            });
+
+            Span name = new Span(user.getName());
+            name.addClassNames("font-medium", "text-s", "text-secondary");
+
+            layout.add(avatar, name);
+        } else {
+            Anchor loginLink = new Anchor("login", "Sign in");
+            layout.add(loginLink);
+        }
+
+        return layout;
+    }
+
+    @Override
+    protected void afterNavigation() {
+        super.afterNavigation();
+        viewTitle.setText(getCurrentPageTitle());
+    }
+
+    private String getCurrentPageTitle() {
+        PageTitle title = getContent().getClass().getAnnotation(PageTitle.class);
+        return title == null ? "" : title.value();
+    }
+}
